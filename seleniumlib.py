d2e49d3b2b5053a1a69cf40d1f994640be08a961
Dyeadal <dyeadal@proton.me>
2025-06-14 17:41:24 -0500

    Update seleniumlib.py

--- seleniumlib.py
+++ seleniumlib.py
@@ -1,5 +1,7 @@
 # Created by dyeadal
 # GNU General Public License 3.0
+from xml.etree.ElementPath import xpath_tokenizer
+
 SeleniumLibVersion = 0.1
 
 from selenium.webdriver.common.by import By # Allows searching for HTML tags
@@ -26,25 +28,35 @@ driver = ucd.Chrome()
 
 # Open URL
 def OpenPage(url):
-    # Print and Log function use
-    PrintAndLog(f"Opening {url} via OpenPage() function.")
-    # Perform HTTP GET of URL
-    driver.get(url)
 
-    # Wait to see page load
-    try:
-        # Wait until condition is met, timeouts after so many seconds set by TimeoutSecToLoad variable
-        WebDriverWait(driver, TimeoutSecToLoad).until(
-            # Condition = Until it can see the <body> tag to see page contents
-            expected_conditions.presence_of_element_located((By.TAG_NAME, "body")) # takes only one argument, double enclosed ()s
-        )
-        PrintAndLog(f"URL successfully loaded, can see <body> tag.")
-        # Wait for a moment before continuing
-        EmulateHumanHesitation()
+    i = input(f"You are attempting to access:\n\n {url}.\n\n Would you like to continue? [Y/n] ")
 
-    # Fails to load in given time
-    except Exception as error:
-        ErrorHandler(f"URL failed to load in time, can't see <body> tag.\n Error: {error}")
+    # User confirms URL is correct and wants to proceed with accessing website
+    if i == "Y" or i == "y":
+        # Print and Log function use of OpenPage()
+        PrintAndLog(f"Opening {url} via OpenPage() function.")
+        # Perform HTTP GET of URL
+        driver.get(url)
+
+        # Wait to see page load
+        try:
+            # Wait until condition is met, timeouts after so many seconds set by TimeoutSecToLoad variable
+            WebDriverWait(driver, TimeoutSecToLoad).until(
+                # Condition = Until it can see the <body> tag to see page contents
+                expected_conditions.presence_of_element_located((By.TAG_NAME, "body")) # takes only one argument, double enclosed ()s
+            )
+            PrintAndLog(f"URL successfully loaded, can see <body> tag.")
+            # Wait for a moment before continuing
+            EmulateHumanHesitation()
+
+        # Fails to load in given time
+        except Exception as error:
+            ErrorHandler(f"URL failed to load in time, can't see <body> tag.\n Error: {error}")
+
+    # Operator chooses no, likely due to URL being incorrect
+    else:
+        PrintAndLog(f"Operator decline to continue to access {url} via OpenPage() function.\n Closing browser.")
+        CloseBrowser()
 
 # Close browser
 def CloseBrowser():
@@ -85,6 +97,10 @@ def EmulateHumanHesitation(min=3.0, max=7.0):
     except Exception as error:
         ErrorHandler(f"Emulating human hesitation failed.\n Error: {error}")
 
+##########################################################################
+### Keyboard Emulation
+##########################################################################
+
 # Insert text as if it were being typed out by a human
 def TypeTextInElement(element, text):
     PrintAndLog(f"Emulating {text} via TypeTextInClass() function in element {element}.")
@@ -94,21 +110,21 @@ def TypeTextInElement(element, text):
     # Loop through each character in the string
     try:
         for item in text:
-            # Send single character to element
-            element.send_keys(item)
             # Create random float variable num
             num = random.random()
-            # Add imitated key pressed and the seconds it took to list
-            seclist = seclist + (f"{item} : {num}, ")
+
             # Wait for random number of milliseconds
             time.sleep(num)
+
+            # Send single character to element
+            element.send_keys(item)
+
+            # Add imitated key pressed and the seconds it took to list
+            seclist = seclist + (f"{item} : {round(num, 2)}, ")
+
         PrintAndLog(f"Successfully imitated a human typing on a keyboard:\n{seclist}\n")
     except Exception as error:
         ErrorHandler(f"Failed imitating typing {text} into element {element}.\n Error: {error}")
-
-##########################################################################
-### Keyboard Emulation
-##########################################################################
 
 # Press Enter key on an element
 def PressEnter(element):
@@ -156,21 +172,30 @@ def InsertText(element, text):
 # Emulate scrolling to an element to be viewable on screen
 def ScrollToElement(element):
     PrintAndLog(f"Scrolling to element {element}.")
+    # contain text in case element is stale
+    txt = element.text
     try:
         driver.execute_script(
             "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
-        PrintAndLog(f"Scrolled to element")
+        PrintAndLog(f"Scrolled to element containing {txt}")
     except Exception as error:
-        ErrorHandler(f"Failed scrolling to element {element}.\n Error: {error}")
+        ErrorHandler(f"\nFailed scrolling to element containing{txt}.\n Error: {error}")
 
 # Click on an element based on Class
 def ClickOnElement(element):
     PrintAndLog(f"Clicking on element {element}.")
     try:
+        #Store elements text as a variable, stale elements caused issues identifying which element is throwing error
+        txt = element.text
+        # Scroll to element to avoid UI issue or errors
+        ScrollToElement(element)
+        # Stop and wait a moment
+        EmulateHumanHesitation()
+        # Click on element
         element.click()
-        print(f"Clicked on element {element.text}")
+        print(f"Clicked on element containing {txt}")
     except Exception as error:
-        ErrorHandler(f"Failed clicking on element {element}.\n Error: {error}")
+        ErrorHandler(f"\nFailed clicking on element containing {txt}.\n Error: {error}")
 
 ##########################################################################
 ### Page Scraping
@@ -179,42 +204,97 @@ def ClickOnElement(element):
 # Find element on webpage by its class name
 def FindElementByClass(classname):
     PrintAndLog(f"Finding element by class {classname}")
+    #
     try:
         element = driver.find_element(By.CLASS_NAME, classname)
         PrintAndLog(f"Found element by class {classname}")
         return element
+    #
     except Exception as error:
         ErrorHandler(f"Failed finding element by class {classname}.\n Error: {error}")
-
+        return None
 
 # Find element on webpage by its ID name
 def FindElementByID(idname):
     PrintAndLog(f"Finding element by ID name {idname}")
+    #
     try:
         element = driver.find_element(By.ID, idname)
         PrintAndLog(f"Found element by ID name {idname}")
         return element
+    #
     except Exception as error:
         ErrorHandler(f"Failed finding element by ID name {idname}\n Error: {error}")
+        return None
+
+# Find the first element by tag and string
+def FindElementByText(tag, string):
+    PrintAndLog(f"Finding element by text name {string} in a {tag} tag.")
+    # Try finding element using string
+
+    # Common attributes that may contain the string the user is identifying in the element they are attempting to interact with
+    common_attr = [
+        'placeholder',
+        'value',
+        'alt',
+        'title',
+        'aria-label'
+    ]
+
+    try:
+        # Build the XPath condition for all common attributes to search the string for
+        attr_conds = " or ".join([
+            f"contains(@{attr}, '{string}')" for attr in common_attr
+        ])
+
+        # Checks if element contain string or attributes that contain the string
+        # normaliza-space removes white spaces to combat messy HTML
+        xpath = f"//{tag}[contains(normalize-space(.), '{string}') or {attr_conds}]"
+        return driver.find_element(By.XPATH, xpath)
+
+    # If no element found with the text, throw error
+    except Exception as error:
+        ErrorHandler(f"Element with tag <{tag}> containing text '{string}' not found.\n Error: {error}.")
+        return None
+
+# Find ALL elements containing a string
+# Useful when string and tag appears throughout your web page
+# Stores results in a list to then be able to filter or interact with individually
+
+def FindAllElementsByText(tag, string):
+    PrintAndLog(f"Finding all elements by text name {string} in a {tag} tag.")
+    try:
+        xpath = f"//{tag}[.//text()[contains(., '{string}')]]"
+        elements = driver.find_elements_by_xpath(xpath)
+        return elements
+    except Exception as error:
+        ErrorHandler(f"Element tag {tag} with text '{string}' not found.\n Error: {error}")
+        return None
 
 # Find element using any By value for advanced users (By.XPATH, By.CSS_SELECTOR, etc.)
 def FindElement(by, value):
     PrintAndLog(f"Finding element by {by} {value}")
+    #
     try:
         element = driver.find_element(by, value)
         PrintAndLog(f"Found element by {by} {value}")
         return element
+    #
     except Exception as error:
         ErrorHandler(f"Failed finding element by {by} {value}\n Error: {error}")
+        return None
 
 # Return text contained in an element
 def ElementText(element):
     PrintAndLog(f"Returning text in element: {element.text}")
+    #
     try:
         PrintAndLog(f"Returning text in element: {element.text}")
         return element.text
+    #
     except Exception as error:
         ErrorHandler(f"Failed returning text in element {element}.\n Error: {error}")
+        return None
 
 # Compare a string to the text contained in an element
 def IfTextInElement(string, element, casesensitive=True):
@@ -241,29 +321,39 @@ def IfTextInElement(string, element, casesensitive=True):
                 ThrowIntentionalError(f"The non-case sensitive string {string} was NOT found in element: {element}.\n Returning False\nElement contains {element.text}")
                 return False
 
+    #
     except Exception as error:
         PrintAndLog(f"Failed to find string {string} in element {element}. Casesensitive: {casesensitive}.\n Error: {error}")
+        return None
+
 ##########################################################################
 ### Debug and Logging
 ##########################################################################
 
+#
 def CurrentTime():
     PrintAndLog(f"Attempting to capture current time via CurrentTime() function.")
+    #
     try:
         raw_time = str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
         PrintAndLog(f"Current time: {raw_time}")
         return raw_time
+    #
     except Exception as error:
         ErrorHandler(f"Failed to capture current time via CurrentTime() function\n Error: {error}")
+        return None
 
 def FormattedCurrentTime():
     PrintAndLog(f"Attempting to format current time via FormattedCurrentTime() function.")
+    #
     try:
         formatted_time = str(datetime.datetime.now().strftime("%m%d%Y_%H%M%S"))
         PrintAndLog(f"Successfully formatted current time: {formatted_time}")
         return formatted_time
+    #
     except Exception as error:
         PrintAndLog(f"Failed to format current time via FormattedCurrentTime() function.\n Error: {error}")
+        return None
 
 # Enable logging, changing global variable. Functions will log actions
 def EnableLogging():
@@ -299,6 +389,7 @@ def CreateLogFile(Directory=os.path.expanduser("~"), NamePrefix="SeleniumLib"):
     # If it fails print out message to terminal and ask if they want to quit or continue without logs
     except Exception as error:
         print(f"Failed to create {filename} log file.\nError: {error}")
+        return None
 
 # Create screenshot
 def Screenshot(NamePrefix="SeleniumLibScreenshot"):
@@ -358,9 +449,11 @@ def WriteToLog(file, msg):
         #Create Log File
         CreateLogFile()
 
-        try: # write log to file
+        # Attempt to write log to file
+        try:
             with open(file, 'a') as file:
                 file.write(CurrentTime() + " --- " + msg + "\n")
+        #
         except Exception as error:
             print(f"An error occurred writing to file: {error}")
 
@@ -376,15 +469,29 @@ def WriteToLog(file, msg):
 def ErrorHandler(msg):
 
     # Print and log error
-    PrintAndLog(f"\nError: \n{msg}\n")
+    PrintAndLog(f"\nError: \n{msg}")
 
-    # Ask the user if they want to continue
-    answer = input("Do you want to continue? [y/n]")
+    # Ask the user if they want to take a screenshot when error occurred
+    i = input("\nDo you want to take a screenshot of the web page when error occurred? [y/n]")
+
+    # if yes take a screenshot and log
+    if i == "y" or i == "Y":
+        PrintAndLog(f"Taking screenshot due to error: {msg}.")
+        Screenshot(NamePrefix="Error")
+        return None
+
+    # If no, logs that no screenshot was saved
+    else:
+        PrintAndLog(f"Operator chose to not save screenshot of webpage when error occurred.")
+
+    # Ask the operator if they want to attempt in continuing despite error
+    j = input("\nDo you want to attempt to continue with existing error? [y/n]")
 
     # If yes, logs the error and the user wants to continue, returns None
-    if answer == "y" or answer == "Y":
+    if j == "y" or j == "Y":
         PrintAndLog(f"User decided to continue after facing Error: {msg}.")
         return None
+
     # If no, logs termination due to error and quits
     else:
         PrintAndLog(f"User decided to terminate script after facing Error: {msg}.")
@@ -392,8 +499,6 @@ def ErrorHandler(msg):
 
 def ThrowIntentionalError(msg):
     PrintAndLog(f" Throwing Intentional Error: \n{msg}\n")
-
-    raise Exception(f"{msg}\n")
 
     # Ask the user if they want to continue
     answer = input("Do you want to continue? [y/n]")
@@ -406,3 +511,4 @@ def ThrowIntentionalError(msg):
     else:
         PrintAndLog(f"\nUser decided to terminate script after facing Error: {msg}.\n")
         CloseBrowser()
+        raise Exception(f"{msg}\n")
